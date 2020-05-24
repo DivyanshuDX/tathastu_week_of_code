f26c1d05591b66fb9747ac4ab6d37221c56ad3fd
DivyanshuDX <64837035+DivyanshuDX@users.noreply.github.com>
2020-05-24 18:29:13 +0530

    Update program1.py

--- Day4/program1.py
+++ Day4/program1.py
@@ -1,1 +1,10 @@
+#to find the no. of occurence in a tuple
 
+size = int(input("Enter the size of tuple: "))
+print("Enter the elements in tuple one by one")
+ar = []
+for i in range(size):
+    ar.append(input())
+ar = tuple(ar)
+element = input("Enter the element whose occurrences you want to know: ")
+print("Tuple contains the element", element , " = " , ar.count(element), "times")
