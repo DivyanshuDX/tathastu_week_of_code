6a8c2330f4383dd2bfb39e8fbda0894dcb029929
DivyanshuDX <64837035+DivyanshuDX@users.noreply.github.com>
2020-05-24 18:06:25 +0530

    Update program4.py

--- Day2/program4.py
+++ Day2/program4.py
@@ -1,1 +1,10 @@
+#to print the SQUARE pattern
 
+n = int(input("Enter the Value of N: "))
+if n <= 1:
+   print("Please enter the no. greater than 1")
+else:   
+   for i in range(n):
+       print("* " * (n - i) + "    " * i + " *" * (n - i))
+   for i in range(2,n + 1):
+       print("* " * i + "    " * (n - i) + " *" * i)
