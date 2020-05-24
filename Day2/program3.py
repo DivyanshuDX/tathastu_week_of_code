81ea054efb5c8592e03d43839a05a9c48457a3e2
DivyanshuDX <64837035+DivyanshuDX@users.noreply.github.com>
2020-05-24 12:43:05 +0530

    Create program3.py

---
+++ Day2/program3.py
@@ -0,0 +1,7 @@
+# to print X pattern
+  
+n = int(input("Enter the value of N: "))
+for i in range(n):
+    print(" " * i + "*" + "  " * (n - 1 - i) + "*")
+for i in range(n):
+    print(" " * (n - 1 - i) + "*" + "  " * i + "*")
