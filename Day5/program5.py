b0f54a053731248915fab0bd2eb9c9e7fbc018ad
Divyanshu_Kumar <dxarmy22091999@gmail.com>
2020-05-26 15:45:00 +0530

    Create program5.py

---
+++ Day5/program5.py
@@ -0,0 +1,16 @@
+#sorting even and odd numbers
+
+def sortend(List):
+    odd = []
+    even = []
+    for x in List:
+        if x % 2 == 0:
+            even.append(x)
+        else :
+            odd.append(x)
+    return sorted(odd, reverse = True) + sorted(even)
+size = int(input("Enter the number of elements you want to add in the array: "))
+List = []
+for i in range(size):
+    List.append(int(input("Enter the element number " + str(i + 1) + " in the list: ")))
+print("The list of numbers after sorting them according to given condition is", str(sortend(List))[1:-1])
